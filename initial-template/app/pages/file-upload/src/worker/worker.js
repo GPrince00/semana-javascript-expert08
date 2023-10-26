f4ffec59678b01f1b4e7a4626d4143b4871f5d67
GPrince00 <gustavoprince0@gmail.com>
2023-10-26 16:19:29 +0700

    feat(worker): setup the configuration of webm

--- initial-template/app/pages/file-upload/src/worker/worker.js
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -1,3 +1,17 @@
+const qvgaConstraints = {
+  width: 320,
+  height: 240,
+};
+
+const encoderConfig = {
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  // WebM
+  codec: "vp09.00.10.08",
+  pt: 4,
+  hardwareAcceleration: "prefer-software",
+};
+
 onmessage = ({ data }) => {
   self.postMessage({
     status: "done",
